4035a3f8891f2554dc910fa4302281e0a1971fd9
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2020-12-21 16:37:40 +0530

    array cube root to json and balanced braces

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,6 +1,23 @@
-
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(args) {
+  let bracesArr = [];
+  for (const currentValue of args.split('')) {
+    if(currentValue === "(" || currentValue === "{" || currentValue === "[") {
+      bracesArr.push(currentValue)
+    }
+    if(currentValue === ")" || currentValue === "}" || currentValue === "]") {
+      let lastBrace = bracesArr[bracesArr.length - 1];
+      if ((lastBrace === "(" && currentValue === ")") || (lastBrace === "[" && currentValue === "]") || (lastBrace === "{" && currentValue === "}")) {
+        bracesArr.pop()
+      } else {
+        bracesArr.push(currentValue)
+      }
+    }
+  }
+  if (bracesArr.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 export {
